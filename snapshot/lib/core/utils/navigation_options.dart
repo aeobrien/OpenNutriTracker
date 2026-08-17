97b8b2e000a5b58e13589f4f4c72071d8406e551
Commit Guardian <guardian@localhost>
2026-08-17 02:24:59 +0100

    Commit Guardian snapshot aidans-laptop 2026-08-17 ONT-wt-integration-64e7cee5477b

---
+++ snapshot/lib/core/utils/navigation_options.dart
@@ -0,0 +1,16 @@
+class NavigationOptions {
+  static const mainRoute = "main";
+  static const onboardingRoute = "onboarding";
+  static const settingsRoute = "settings";
+  static const addMealRoute = "addMeal";
+  static const scannerRoute = "scanner";
+  static const mealDetailRoute = "mealDetail";
+  static const editMealRoute = "editMeal";
+  static const addActivityRoute = "addActivity";
+  static const activityDetailRoute = "activityDetail";
+  static const imageFullScreenRoute = "imageFullScreen";
+  static const quickAddRoute = "quickAdd";
+  static const labelScanRoute = "labelScan";
+  static const recipeLogRoute = "recipeLog";
+  static const llmRecipeRoute = "llmRecipe";
+}
